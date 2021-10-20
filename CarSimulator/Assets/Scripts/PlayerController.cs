cca6616f292094c0cc79bb61a5304a3e3666c6d0
egemen6k <egemen.altikardesler@gmail.com>
2021-10-20 12:36:39 +0300

    +wheel colliders & onground

--- CarSimulator/Assets/Scripts/PlayerController.cs
+++ CarSimulator/Assets/Scripts/PlayerController.cs
@@ -6,23 +6,21 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float rpm;
     [SerializeField] private float horsePower = 0;
     [SerializeField] private float turnSpeed = 40f;
     private float horizontalInput,forwardInput;
     private Rigidbody _rbPlayer;
     [SerializeField] private GameObject _centerOfMass;
     [SerializeField] private TextMeshProUGUI speedometerText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    [SerializeField] private List<WheelCollider> wheelColliders;
+    [SerializeField] private int wheelsOnGround;
 
     private void Start()
     {
         _rbPlayer = GetComponent<Rigidbody>();
         _rbPlayer.centerOfMass = _centerOfMass.transform.position;
-    }
-
-    private void Update()
-    {
-        speed = Mathf.Round(_rbPlayer.velocity.magnitude * 2.237f);
-        speedometerText.text = "Speed: " + speed + "mph";
     }
 
     // Update is called once per frame
@@ -32,8 +30,40 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         /*Vector3 force = transform.TransformDirection(Vector3.forward * forwardInput * horsePower);
         _rbPlayer.AddForce(force); */
-        _rbPlayer.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
-        //transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
-        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+
+        if (isOnGround())
+        {
+            _rbPlayer.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+            //transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.text = "RPM: " + rpm;
+
+            speed = Mathf.Round(_rbPlayer.velocity.magnitude * 2.237f);
+            speedometerText.text = "Speed: " + speed + "mph";
+        }
+    }
+
+    bool isOnGround()
+    {
+        wheelsOnGround = 0;
+
+        foreach (WheelCollider wheel in wheelColliders)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
