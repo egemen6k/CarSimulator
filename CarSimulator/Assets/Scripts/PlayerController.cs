3cf948beaf50bb613e8bb01f9c36eba91d3da9c3
egemen6k <egemen.altikardesler@gmail.com>
2021-10-20 11:40:46 +0300

    +RelativeForce implementation

--- CarSimulator/Assets/Scripts/PlayerController.cs
+++ CarSimulator/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 20f;
+    //[SerializeField] private float speed = 20f;
+    [SerializeField] private float horsePower = 0;
     [SerializeField] private float turnSpeed = 40f;
     private float horizontalInput,forwardInput;
+    private Rigidbody _rbPlayer;
+
+    private void Start()
+    {
+        _rbPlayer = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+        /*Vector3 force = transform.TransformDirection(Vector3.forward * forwardInput * horsePower);
+        _rbPlayer.AddForce(force); */
+        _rbPlayer.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+        //transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
         transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
     }
 }
